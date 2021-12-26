25a38fd164db52f8540ffa72406d921c6ba0f86e
KonstantinSavka <50841988+KonstantinSavka@users.noreply.github.com>
2021-12-26 19:59:08 +0200

    test

---
+++ dz/api.js
@@ -0,0 +1,94 @@
+const api = (() => {
+    const url = 'https://reqres.in/api';
+    class Api {
+        // constructor() {
+        //     // this._xhr = new XMLHttpRequest();
+        // }
+
+        login(email, password) {
+            return fetch(`${url}/login`, {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email,
+                    password
+                })
+            }).then(response => response.json());
+        }
+
+        // loginPr(email, password) {
+        //     const request = new Promise((resolve, reject) => {
+        //         this._xhr.open('POST', `${url}/login`);
+        //         this._xhr.setRequestHeader('content-type', 'application/json');
+        //         this._xhr.onload = (e) => {
+        //             const response = JSON.parse(e.currentTarget.responseText);
+        //             if (response.token) {
+        //                 resolve(response);
+        //             } else {
+        //                 reject(response);
+        //             }
+        //         };
+        //         this._xhr.send(JSON.stringify({
+        //             email,
+        //             password
+        //         }));
+        //     });
+        //     return request;
+        // }
+
+        // loginCb(email, password, responseCb) {
+            //     this._xhr = new XMLHttpRequest();
+        //         this._xhr.open('POST', `${url}/login`);
+        //         this._xhr.setRequestHeader('content-type', 'application/json');
+
+        //         this._xhr.onload = (e) => {
+        //             const response = JSON.parse(e.currentTarget.responseText);
+        //             responseCb && responseCb(response);
+        //         };
+        //         this._xhr.send(JSON.stringify({
+        //             email,
+        //             password
+        //         }));
+        //     }
+
+
+
+        // getUsersCb(page = 1, callback) {
+
+        //     this._xhr.open('GET', `${url}/users?page=${page}`, true);
+        //     this._xhr.onload = (e) => {
+        //         const { data, total_pages } = JSON.parse(e.currentTarget.responseText);
+        //         callback && callback(data, total_pages);
+        //     }
+        //     this._xhr.send();
+        // };
+
+
+        getUsers(page = 1) {
+            return fetch(`${url}/users?page=${page}`).then(response => response.json())
+        };
+
+
+        // getUsers(page = 1) {
+        //     const request = new Promise((resolve, reject) => {
+        //         this._xhr.open('GET', `${url}/users?page=${page}`, true);
+        //         this._xhr.onload = (e) => {
+        //             const { data, total_pages } = JSON.parse(e.currentTarget.responseText);
+        //             if(data && total_pages) {
+        //                 resolve([data, total_pages])
+        //             } else {
+        //                 reject(e.currentTarget.responseText)
+        //             }
+        //         }
+        //         this._xhr.send();
+        //     });
+        //     return request;
+        // };
+    }
+
+    return new Api();
+})();
+
+export {api};
---
+++ dz/login.js
@@ -0,0 +1,41 @@
+import { api } from './api.js';
+import { render } from './helpers.js';
+
+class LoginComponent {
+    constructor(template, containerEl) {
+        this._template = template;
+        this._containerEl = containerEl;
+        this.onSuccess = () => {};
+        this.render();
+    }
+
+    onSubmit() {
+        const email = this._containerEl.querySelector('#email_field').value;
+        const password = this._containerEl.querySelector('#password_field').value;
+        // api.loginCb(email, password, (response) => {
+        //     if(response.token) {
+        //         this.onSuccess && this.onSuccess(response.token);
+        //     }
+        // });
+        // api.loginPr(email, password)
+        // .then(e => {
+        //     this.onSuccess && this.onSuccess(e.token);
+        // });
+        api.login(email, password)
+        .then(e => {
+            this.onSuccess && this.onSuccess(e.token);
+            localStorage.setItem('token', e.token);
+        });
+    }
+
+    render() {
+        if(!this._containerEl) return;
+        this._containerEl.innerHTML = render(this._template, {});
+        this._containerEl.addEventListener('click', (e) => {
+            if(e.target.classList.contains('buttonSend')) this.onSubmit(e)
+        });
+
+    }
+}
+
+export {LoginComponent};
