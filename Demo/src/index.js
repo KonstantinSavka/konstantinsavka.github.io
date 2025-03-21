d6b4c4ecd83bd96ffe01add878d098c15838bc30
KonstantinSavka <bronwinar@gmail.com>
2025-03-21 16:00:19 +0200

    index update

--- Demo/src/index.js
+++ Demo/src/index.js
@@ -4,7 +4,7 @@ import reportWebVitals from './reportWebVitals';
 import store from "./redux/reduxStore";
 import ReactDOM from "react-dom/client";
 import React from "react";
-import {BrowserRouter} from "react-router-dom";
+import {BrowserRouter, HashRouter} from "react-router-dom";
 import App from "./App";
 import {Provider} from "react-redux";
 import axios from "axios";
@@ -13,11 +13,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App />
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>
 );
 
