9f087c7240da5c8fc0566dc3e5d58b7debdf6b23
KonstantinSavka <bronwinar@gmail.com>
2025-03-21 16:21:50 +0200

    test

--- Demo/src/components/Nav/Nav.jsx
+++ Demo/src/components/Nav/Nav.jsx
@@ -5,7 +5,7 @@ const Nav = () => {
     return (<nav className='md:col-span-1 md:flex md:justify-end text-right'>
             <div className='text-sm mt-6 hidden md:block'>
                 <NavLink to="/users" className={({ isActive }) => isActive ? 'text-gray-700 border-r-4 border-red-400 font-bold py-1 block' : "py-1 block border-r-4 border-transparent"}>
-                    <span className='px-4 flex justify-end'>Users</span>
+                    <span className='px-4 flex justify-end'>Usersss</span>
                 </NavLink>
                 <NavLink to="/newUser" className={({ isActive }) => isActive ? 'text-gray-700 border-r-4 border-red-400 font-bold py-1 block' : "py-1 block border-r-4 border-transparent"}>
                     <span className='px-4 flex justify-end'>Create user</span>
